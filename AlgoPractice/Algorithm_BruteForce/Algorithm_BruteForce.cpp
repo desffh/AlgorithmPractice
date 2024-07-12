959ddd7a42a9f4c7637c76063794f6be00e60c28
chichi <yangju058@gmail.com>
2024-07-12 16:04:05 +0900

    (2024.7.11)알고리즘1 복습
    1. 시간 복잡도
    2. 브루트 포스
    3. 소수 판별

---
+++ AlgoPractice/Algorithm_BruteForce/Algorithm_BruteForce.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+#pragma region 브루트 포스
+    // 모든 경우의 수를 탐색하면서 
+    // 결과를 도출하는 알고리즘
+
+    // 비밀번호3자리 찾기 (000 ~999)까지
+
+    // 1. 비밀번호 1차원 배열 만들기
+    int Password[] = { 2,4,8 };
+
+    // 시간복잡도: 0(n³)
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            for (int k = 0; k < 10; k++)
+            {
+                if (Password[0] == i && Password[1] == j
+                    && Password[2] == k)
+                {
+                    cout << "Open" << endl;
+                    cout << i << " " << j << " " << k << " ";
+                }
+            }
+        }
+    }
+
+#pragma endregion
---
+++ AlgoPractice/Algorithm_IsPrime/Algorithm_IsPrime.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+// 소수 판별하기
+// 소수가 맞다면 true 1 / 소수가 아니라면 false 0
+
+bool Number(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true; // 예외를 제외한 나머지는 소수
+}
+
+
+int main()
+{
+#pragma region 소수 판별하기
+    cout << Number(1); // 소수아님 0
+    cout << Number(3); // 소수맞음 1
+#pragma endregion
+
+
+    return 0;
+}
+
