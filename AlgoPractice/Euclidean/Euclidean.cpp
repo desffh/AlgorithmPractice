6eceb1bdf2fb6257ddcc5518b6ceffe59357fae3
desffh <163123142+desffh@users.noreply.github.com>
2024-08-03 05:38:22 +0900

    (2024.8.3) 백업파일

---
+++ AlgoPractice/Euclidean/Euclidean.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+// 유클리드 호제법
+
+// 큰수 int x;
+// 작은수 int y;
+
+//1. x % y = 몫 --- 나머지 y1
+
+//2. y % 나머지 y1 = 몫 --- 나머지 y2
+
+//3. y1 % y2 = 몫 --- 나머지 y3
+
+//4. y2 % y3 = 몫 --- 나머지 0
+//		x    y
+//최종: y3 % 0 = 일때 y는 0, 최대공약수인 x를 반환
+
+// 나머지가 0일때의 y값이 최대공약수
+// y3 = 최대공약수
+
+int Euclid(int x, int y)
+{
+	// 더이상 나누어 떨어지지 않음 y = 최대공약수
+	if (y == 0)
+	{
+		return x;
+	}
+	else
+	{
+		return Euclid(y, x % y);
+	}
+}
+
+
+int main()
+{
+	int x = 12300;
+	int y = 55400;
+
+	cout << "두 수의 최대 공약수 :";
+
+	// x,y중 큰 값은 max, 작은 값은 min
+	// 서로 위치가 바뀌지 않게 설정됨
+	cout << Euclid(max(x, y), min(x, y)) << endl;
+
+
+	return 0;
+}
---
+++ AlgoPractice/최대공약수/최대공약수.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+
+// 최대공약수
+// 두 수가 동시에 나누어 떨어지는 공통된 약수 중 가장 큰 수
+
+
+int main()
+{
+	int x = 30;
+	int y = 48;
+
+	int num = 0;
+
+	// min(x, y) = x와 y중 더 작은 값을 반환
+	for (int i = 1; i <= min(x, y); i++)
+	{
+		if (x % i == 0 && y % i == 0)
+		{
+			num = i;
+		}
+	}
+	cout << "최대공약수의 값: " << num << endl;
+
+	return 0;
+}
