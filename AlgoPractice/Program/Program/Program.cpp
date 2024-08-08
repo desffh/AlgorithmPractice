bb8529c57eb900f7b213c3fdba585cbf5c80661c
chichi <yangju058@gmail.com>
2024-08-08 17:43:28 +0900

    (2024.8.8) 신장트리
    최소신장트리
    (사이클x)

--- AlgoPractice/Program/Program/Program.cpp
+++ AlgoPractice/Program/Program/Program.cpp
@@ -1,75 +1,104 @@
 ﻿#include <iostream>
+#include <vector>
+#include <algorithm> // sort사용하려면 
+
 using namespace std;
-#define SIZE 7
+#define SIZE 8
 
-// 유니온 파인드
-// 여러 노드가 있을때 어떤 노드가 어떤 노드와 연결되어 있는지
-// 확인하는 알고리즘
-
+// 그래프
 class Graph
 {
 private:
 	int parent[SIZE]; // 부모배열
 
+	int cost;
+	
+	// 간선
+	class Edge
+	{
+	// 연결할 노드 x와 y, 그 사이 가중치
+	private:
+		int x;
+		int y;
+		int distance;
+
+	public:
+		// 초기화
+		// main함수에서 받은 인수값을 매개변수로 받아
+		// 멤버변수 각각에 할당
+		Edge(int x, int y, int distance)
+		{
+			this->x = x;
+			this->y = y;
+			this->distance = distance;
+		}
+		int& X()
+		{
+			return x;
+		}
+		int& Y()
+		{
+			return y;
+		}
+		int& Distance()
+		{
+			return distance;
+		}
+
+
+	};
+
+	vector <Edge> graph; // Edge를 담을 vector형 배열
 
 public:
 	Graph()
 	{
-		// 부모배열 = 부모배열 값으로 초기화
+		// 부모배열 초기화
 		for (int i = 1; i < SIZE; i++)
 		{
 			parent[i] = i;
 		}
 	}
-	// 부모찾는 함수
+	// vector배열에 정점과 간선 삽입
+	void Insert(int x, int y, int distance)
+	{
+		// graph배열에 Edge넣어주기
+		graph.push_back(Edge(x, y, distance));
+	}
+
+	// 부모 노드 찾기
 	int Find(int x)
 	{
-		// 부모배열 == 부모배열의 값
 		if (parent[x] == x)
 		{
-			return x; // root노드를 반환
+			return x;
 		}
-		else
-		{
-			// 재귀를 돌려 부모배열 찾아서 return
-			parent[x] = Find(parent[x]);
-			return parent[x];
-		}
+
 	}
 
-	// 합치기
-	void Union(int x, int y)
-	{
-		// 루트노드 x, y 찾기
-		x = Find(x);
-		y = Find(y);
 
-		// 노드의 값을 합쳐준다
-		if (x < y)
-		{
-			parent[y] = x;
-		}
-		else
-		{
-			parent[x] = y;
-		}
-	}
-
-	// 부모노드가 서로 같은지 확인
-	bool Same(int x, int y)
-	{
-		return Find(x) == Find(y);
-		// 같으면 true, 틀리면 false값을 반환
-	}
 };
+
+
 
 
 int main()
 {
 	Graph graph;
 
-	graph.Union(2, 3);
-	graph.Union(1, 3);
+	graph.Insert(1, 7, 10);
+	graph.Insert(1, 4, 28);
+	graph.Insert(1, 2, 66);
+	graph.Insert(1, 5, 19);
+
+	graph.Insert(2, 4, 25);
+	graph.Insert(2, 5, 60);
+
+	graph.Insert(3, 5, 21);
+	graph.Insert(3, 6, 35);
+
+	graph.Insert(4, 7, 15);
+	graph.Insert(5, 6, 40);
 
 
 	return 0;
---
+++ AlgoPractice/Union Find/Union Find.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+using namespace std;
+#define SIZE 7
+
+// 유니온 파인드
+// 여러 노드가 있을때 어떤 노드가 어떤 노드와 연결되어 있는지
+// 확인하는 알고리즘
+
+// Union: x집합과 y집합끼리 트리구조로 합치기 
+// Find: x가 속한 집합의 대표값 반환(부모노드)
+
+class Graph
+{
+private:
+	int parent[SIZE]; // 부모배열
+
+
+public:
+	Graph()
+	{
+		// 부모배열 = 부모배열 값으로 초기화
+		for (int i = 1; i < SIZE; i++)
+		{
+			parent[i] = i;
+		}
+	}
+	// 부모찾는 함수
+	int Find(int x)
+	{
+		// 부모배열 == 부모배열의 값
+		if (parent[x] == x)
+		{
+			return x; // root노드를 반환
+		}
+		else
+		{
+			// parent[x]의 값을 Find()에 넣어 재귀를 돌리고
+			// 반환된 x값을 넣어주기
+			// ex) parent[x] = 반환된 x값
+			// return parent[x]해주기
+
+			parent[x] = Find(parent[x]);
+			return parent[x];
+		}
+	}
+
+	// 합치기
+	void Union(int x, int y)
+	{
+		// 루트노드 x, y 찾기
+		x = Find(x);
+		y = Find(y);
+
+		// 노드의 값을 root노드로 바꿔주기
+		if (x < y)
+		{
+			parent[y] = x;
+		}
+		else
+		{
+			parent[x] = y;
+		}
+	}
+
+	// 부모노드가 서로 같은지 확인
+	bool Same(int x, int y)
+	{
+		return Find(x) == Find(y);
+		// 같으면 true 0, 틀리면 false 1값을 반환
+	}
+};
+
+
+int main()
+{
+	Graph graph;
+
+	graph.Union(2, 3);
+	graph.Union(1, 3);
+
+	cout << graph.Same(1, 3); // 1
+	cout << graph.Same(2, 3); // 1
+	cout << graph.Same(3, 4); // 0
+
+	return 0;
+}
