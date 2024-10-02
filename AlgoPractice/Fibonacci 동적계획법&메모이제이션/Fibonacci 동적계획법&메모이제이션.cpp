a390f8bbfd5b2599d25d98d4ff2dc0619e1c670a
desffh <yangju058@gmail.com>
2024-10-02 14:47:03 +0900

    (2024.10.2) 동적계획 & Greedy
    1. 동적계획법 & 메모이제이션
    2. Greedy 탐욕법

---
+++ AlgoPractice/Fibonacci 동적계획법&메모이제이션/Fibonacci 동적계획법&메모이제이션.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+#define SIZE 1000
+
+// 피보나치 수열
+
+int Fibonacci(int n, int list[])
+{
+	if (n <= 0)
+	{
+		return 0;
+	}
+	else if (n <= 2) // 1,2번째 값은 1
+	{
+		return 1;
+
+	// 이미 계산된 값이 list에 저장되어있다 (재사용 가능)
+	if (list[n] != 0)
+	{
+		return list[n];
+	}
+
+	// 값을 저장
+	return list[n] = Fibonacci(n - 1, list) + Fibonacci(n - 2, list);
+
+}
+
+
+
+int main()
+{
+	// 동적계획법
+	// -> 특정 범위 값을 구하기 위해서, 다른 범위의 값을 이용해 값을 구하는 효율적인 알고리즘
+
+
+	// 메모이제이션 기법
+	// 동일한 계산을 반복해야할 때, 이전에 계산한 값을 메모리에 저장하여,
+	// 동일한 계산을 피하고, 계산속도를 향상시켜주기 위한 기법
+
+	int list[SIZE] = { 0, }; // list 모두 0으로 초기화
+
+	cout << Fibonacci(6, list);
+
+	return 0;
+}
--- AlgoPractice/Program/Program/Program.cpp
+++ AlgoPractice/Program/Program/Program.cpp
@@ -1,11 +1,58 @@
 ﻿#include <iostream>
 using namespace std;
-#define SIZE 8
+
+// 탐욕법
+// -> 최적의 해를 구하는데 사용되는 방식
+// -> "그 순간에" 최적이라고 생각되는 것을 선택해나감
+// -> 단순해서 실행시간이 빠름
+
+int Greedy(int n)
+{
+	int count = 0;
+
+	while (n != 0)
+	{
+		if (n >= 1000)
+		{
+			n -= 1000;
+			count++;
+		}
+	    else if (n >= 500)
+		{
+			n -= 500;
+			count++;
+		}
+		else if (n >= 100)
+		{
+			n -= 100;
+			count++;
+		}
+		else if (n >= 50)
+		{
+			n -= 50;
+			count++;
+		}
+		else if (n >= 10)
+		{
+			n -= 10;
+			count++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return count;
+
+}
 
 
 
 int main()
 {
+	cout << Greedy(1340);
+	// 결과값: 1000 1개, 100 3개, 10 4개
+
 
 	return 0;
 }
